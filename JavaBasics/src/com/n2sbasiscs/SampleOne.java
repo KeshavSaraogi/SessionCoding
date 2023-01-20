eb29fcf6f3d03ff6a851d5b0e1b53af4ad77ef72
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-01-20 00:04:29 -0500

    functions

--- JavaBasics/src/com/n2sbasiscs/SampleOne.java
+++ JavaBasics/src/com/n2sbasiscs/SampleOne.java
@@ -6,5 +6,26 @@ public class SampleOne {
 		System.out.println("Hello World One");
 
 	}
+	
+	public int add(int x, int y) {
+		return x + y;
+	}
+	
+	public int subtract(int x, int y) {
+		return x - y;
+	}
+	
+	public int multiply(int x, int y) {
+		return x * y;
+	}
+	
+	public int divide(int x, int y) {
+		if (x == 0 || y == 0) {
+			return -1;
+		}
+		else {
+			return x / y;
+		}
+	}
 
 }
