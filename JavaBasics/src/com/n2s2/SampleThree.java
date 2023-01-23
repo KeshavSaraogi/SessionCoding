bc05539690bfe74d85c1603cc97690362340c11c
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-01-22 19:28:22 -0500

    outside package

---
+++ JavaBasics/src/com/n2s2/SampleThree.java
@@ -0,0 +1,13 @@
+package com.n2s2;
+
+import com.n2sbasiscs.SampleOne;
+
+public class SampleThree {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SampleOne sample = new SampleOne();
+		System.out.println(sample.age);
+	}
+
+}
