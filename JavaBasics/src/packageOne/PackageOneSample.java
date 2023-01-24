52885e63ba2d560d0561ba91a368e7d4629ffd24
Keshav Saraogi <78319655+KeshavSaraogi@users.noreply.github.com>
2023-01-24 13:27:09 -0500

    protected

---
+++ JavaBasics/src/packageOne/PackageOneSample.java
@@ -0,0 +1,13 @@
+package packageOne;
+
+public class PackageOneSample {
+
+	
+	public void methodOne() {
+		System.out.println("Method One");
+	}
+	
+	protected void methodTwo() {
+		System.out.println("Method Two");
+	}
+}
---
+++ JavaBasics/src/packageOne/PackageOneTest.java
@@ -0,0 +1,10 @@
+package packageOne;
+
+public class PackageOneTest {
+
+	public static void main(String[] args) {
+		PackageOneSample pos = new PackageOneSample();
+		pos.methodOne();
+		pos.methodTwo();
+	}
+}
